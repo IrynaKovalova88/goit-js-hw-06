bb53c3902f0784b1ef41b9ec282495879c1b7d68
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-16 00:25:10 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemIngredients = document.querySelector('#ingredients');
+ingredients.forEach(ingredient => {
+  const listIngredients = document.createElement('li');
+  listIngredients.classList.add('item');
+  listIngredients.textContent = ingredient;
+  itemIngredients.appendChild(listIngredients);
+})
