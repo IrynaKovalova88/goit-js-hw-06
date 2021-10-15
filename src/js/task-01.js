644b3eb48c95e02e33151511e1234086a3ca7df8
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-15 23:46:54 +0300

    task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const numberCategories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${numberCategories.length}`);
+
+numberCategories.forEach(el => {
+  console.log(
+    `Category: ${el.querySelector('h2').textContent}`
+    );
+    console.log(`Elements: ${el.querySelectorAll('li').length}`);
+});
