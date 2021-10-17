b4dc7441c9962abbc9c3d0327fb6c2e8117f6370
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-17 18:36:19 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const textColor = document.querySelector('.color');
+const randomColor = document.querySelector('.change-color');
+
+const onChangeColor = () => {
+  const currentColor = getRandomHexColor();
+  textColor.textContent = currentColor;
+  body.style.backgroundColor = currentColor;
+};
+randomColor.addEventListener('click', onChangeColor);
