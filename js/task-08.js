d6b5b18a39ae86b7cc9cf449dc3397006083aa28
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-17 18:26:37 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector('form.login-form');
+const onSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
+    if (!email || !password) return alert('Все поля должны быть заполнены!');
+
+    console.log({ email, password });
+    loginForm.reset();
+}
+loginForm.addEventListener('submit', onSubmit);
