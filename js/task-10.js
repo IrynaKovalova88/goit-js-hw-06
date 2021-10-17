d8a835e5192bc623df4abf158613f5009ce7c6ba
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-17 18:43:23 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('#controls input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+const createBoxes = (amount) => {
+  amount = input.value;
+
+  for (let index = 0; index < amount; index += 1) {
+    const baseSize = 30;
+    const biggestSize = baseSize + index * 10;
+    const box = document.createElement('div');
+    box.setAttribute('style', `width: ${biggestSize}px; height: ${biggestSize}px; background: ${getRandomHexColor()}`);
+    boxes.append(box);
+  }
+}
+const destroyBoxes = () => {
+  boxes.innerHTML = '';
+}
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
